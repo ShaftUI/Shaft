da118ee9d6e523b595ff2b9d24756115b220c8c3
xuty <xty50337@hotmail.com>
2025-04-18 15:45:04 +0800

    Fix build

--- Sources/Fetch/Fetch.swift
+++ Sources/Fetch/Fetch.swift
@@ -1,18 +1,17 @@
 import Foundation
 
-#if canImport(JavaScriptKit)
-    import JavaScriptKit
-#elseif canImport(FoundationNetworking)
+#if canImport(FoundationNetworking)
     import FoundationNetworking
+#elseif os(wasi)
+    import JavaScriptKit
 #endif
 
 /// Fetches data from a URL.
 public func fetch(_ url: URL) async throws -> Data {
-    #if canImport(JavaScriptKit)
+    #if os(wasi)
         let jsFetch = JSObject.global.fetch.function!
         let response = try await JSPromise(jsFetch(url.absoluteString).object!)!.jsValue
         let buffer = try await JSPromise(response.arrayBuffer().object!)!.jsValue
-        print(buffer)
         return Data()
     #else
         return try await URLSession.shared.data(from: url).0
