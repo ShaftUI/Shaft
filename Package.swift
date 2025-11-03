6421520ccb2cf7a8cc026b42753a1abff8f1ad3b
tyxu <xty50337@hotmail.com>
2025-11-03 17:44:39 +0800

    Update SwiftSDL3 dependency to version 0.1.2

--- Package.swift
+++ Package.swift
@@ -52,7 +52,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/ShaftUI/SwiftSDL3",
-            .upToNextMinor(from: "0.1.1")
+            exact: "0.1.2"
         ),
         // .package(
         //     name: "SwiftSDL3",
